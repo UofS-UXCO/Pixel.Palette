ce2a8226079fab82d778a5d4ba33673aa7abc9f2
Vinh <tip434@usask.ca>
2024-03-24 23:49:37 -0600

    Update layout

--- script/script.js
+++ script/script.js
@@ -1,8 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var hamburger = document.querySelector('.hamburger-menu');
-  var mobileMenu = document.querySelector('.mobile-menu');
+// X-axis scroll-able slider.
+//document.addEventListener("wheel", function(e) {
+  //const deltaX = e.deltaY;
+  //const slider = document.querySelector('.img-slider-wrapper');
+  //slider.scrollLeft += deltaX;
+//});
 
-  hamburger.addEventListener('click', function() {
-    mobileMenu.classList.toggle('active');
-  });
+//Auto Gallery Slider.
+document.addEventListener('DOMContentLoaded', (event) => {
+  let slideIndex = 0; // Start with the first image
+  showSlides();
+
+  function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("slide");
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      slides[slideIndex-1].style.display = "block";  
+      setTimeout(showSlides, 5000); // Change image every 5 seconds
+  }
 });
